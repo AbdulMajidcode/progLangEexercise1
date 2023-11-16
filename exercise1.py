4a31dda92ef6a592aea78fc4d73dbfe78893115d
AbdulMajidcode <119073141+AbdulMajidcode@users.noreply.github.com>
2023-11-16 15:39:44 +0300

    Add files via upload

---
+++ exercise1.py
@@ -0,0 +1,8 @@
+# SCT211-0028/2022
+# ABDIMAJID DAKANE MUKTAR
+name = input("Enter your name: ")
+print("Hello ", name)
+number1 = int(input("Enter the first number: "))
+number2 = int(input("Enter second number:"))
+sum = number1 + number2
+print("The sum of ",number1, "and " , number2 , "is: " , sum)
